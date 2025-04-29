0a0fc0add69c8cbcc5836fd0c140c236a169820b
SalinasJason <jasonsalinas770@gmail.com>
2025-04-28 21:25:56 -0700

    Added step 2

---
+++ COMP003B.LectureActivity6.1/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿namespace COMP003B.LectureActivity6._1.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        // Collection navigation property
+
+        public virtual ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
